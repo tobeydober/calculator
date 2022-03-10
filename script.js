9965718e722ad480bae3a87b75871f0b02bd7b43
Patrick <patrickhan37@gmail.com>
2022-03-10 00:28:37 -0500

    testing with grid lines

---
+++ script.js
@@ -0,0 +1,17 @@
+const ADD = ((number1, number2) => number1 + number2)
+const SUBTRACT = ((number1, number2) => number1 - number2)
+const MULTIPLY = ((number1, number2) => number1 * number2)
+const DIVIDE = ((number1, number2) => number1 / number2)
+
+const OPERATE = (function(number1, number2, operator) {
+    switch (operator) {
+        case '+':
+            return ADD(number1, number2);
+        case '-':
+            return SUBTRACT(number1, number2);
+        case '*':
+            return MULTIPLY(number1, number2);
+        case '/':
+            return DIVIDE(number1, number2)
+    }
+})
