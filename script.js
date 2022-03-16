af17dba05ce84547291d36935c036722671d2c23
Patrick <patrickhan37@gmail.com>
2022-03-16 01:05:45 -0400

    finished calculator

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const BUTTOMSCREEN = document.querySelector('.bottomScreen')
+const TOPSCREEN = document.querySelector('.topScreen')
+const DELETEBUTTON = document.querySelector('.deleteButton')
+const RESETBUTTON = document.querySelector('.resetButton')
+const BUTTONS = document.querySelectorAll('.button')
+let firstNumber
+let secondNumber
+let operator 
+let counter = false //this makes sure equal button can't  be press multiples times in a row
+
 const ADD = ((number1, number2) => number1 + number2)
 const SUBTRACT = ((number1, number2) => number1 - number2)
 const MULTIPLY = ((number1, number2) => number1 * number2)
@@ -16,83 +26,93 @@ const OPERATE = (function(number1, number2, operator) {
             return DIVIDE(number1, number2)
     }
 })
-const BUTTOMSCREEN = document.querySelector('.bottomScreen')
-const TOPSCREEN = document.querySelector('.topScreen')
-const DELETEBUTTON = document.querySelector('.deleteButton')
-const RESETBUTTON = document.querySelector('.resetButton')
-const BUTTONS = document.querySelectorAll('.button')
-let firstNumber
-let secondNumber
-let operator 
-
-const RESET = function() {
+const RESET = () => {
     RESETBUTTON.addEventListener('click', function() {
         BUTTOMSCREEN.textContent = ''
         TOPSCREEN.textContent = ''
         firstNumber = ''
         secondNumber = ''
+        operator = ''
     })
 }
 
+//deletes last element in string
 const DELETE = function() {
     DELETEBUTTON.addEventListener('click', function() {
         return BUTTOMSCREEN.textContent = BUTTOMSCREEN.textContent.slice(0, -1)
     })
 }
 
-BUTTONS.forEach(function(button) {
-    button.addEventListener('click', function(){
-        let x = button.innerText
-        switch (x) {
-            case '/':
-                firstNumber = BUTTOMSCREEN.innerText
-                TOPSCREEN.textContent = `${BUTTOMSCREEN.textContent} ${button.innerText}`
-                BUTTOMSCREEN.textContent = ''
-                operator = button.innerText
-                return
-            case '*':
-                firstNumber = BUTTOMSCREEN.innerText
-                TOPSCREEN.textContent = `${BUTTOMSCREEN.textContent} x`
-                BUTTOMSCREEN.textContent = ''
-                operator = button.innerText
-                return
-            case '-':
-                firstNumber = BUTTOMSCREEN.innerText
-                TOPSCREEN.textContent = `${BUTTOMSCREEN.textContent} ${button.innerText}`
-                BUTTOMSCREEN.textContent = ''
-                operator = button.innerText
-                return
-            case '+':
-                firstNumber = BUTTOMSCREEN.innerText
-                TOPSCREEN.textContent = `${BUTTOMSCREEN.textContent} ${button.innerText}`
-                BUTTOMSCREEN.textContent = ''
-                operator = button.innerText
-                return
-            case '=':
-                console.log(typeof(firstNumber))
-                secondNumber = BUTTOMSCREEN.innerText
-                TOPSCREEN.textContent = `${firstNumber} ${operator} ${secondNumber}`
-                BUTTOMSCREEN.textContent = OPERATE(firstNumber,secondNumber,operator)
-                return
-            default:
-                BUTTOMSCREEN.textContent += button.innerText
-                console.log('test')
-        }
-        // if (button.innerText === '/' ||
-        //     button.innerText === '*' ||
-        //     button.innerText === '-' ||
-        //     button.innerText === '+' ) {
-        //     TOPSCREEN.textContent = `${BUTTOMSCREEN.textContent} ${button.innerText}`
-        //     BUTTOMSCREEN.textContent = ''
-        //     return
-        // }
-        // BUTTOMSCREEN.textContent += button.innerText
+//helps clean OPERATERBUTTONS function
+const OPERATERHELPER = (operatorButton) => {
+    firstNumber = BUTTOMSCREEN.innerText
+    TOPSCREEN.textContent = `${BUTTOMSCREEN.textContent} ${operatorButton.innerText}`
+    BUTTOMSCREEN.textContent = ''
+    operator = operatorButton.innerText
+    counter = true
+}
+
+//pushes number in screen up and records the operator pressed
+//takes in actual div of button for its textcontent
+const OPERATERBUTTONS = function(operatorButton) {
+    console.log(BUTTOMSCREEN.innerHTML)
+    if (!BUTTOMSCREEN.textContent) {
+        return
+    }
+    if (operator) { //this makes it work if the user presses a operator button instead of equal after
+        secondNumber = BUTTOMSCREEN.innerText
+        TOPSCREEN.textContent = `${firstNumber} ${operator} ${secondNumber}`
+        BUTTOMSCREEN.textContent = OPERATE(firstNumber,secondNumber,operator)
+        OPERATERHELPER(operatorButton)
+        return
+    }
+    OPERATERHELPER(operatorButton)
+    return
+}
+
+const MAINBUTTONS = function() {
+    BUTTONS.forEach(function(button) {
+        button.addEventListener('click', function(){
+            let x = button.innerText
+            switch (x) {
+                case '/':
+                    OPERATERBUTTONS(button)
+                    return
+                case '*':
+                    OPERATERBUTTONS(button)
+                    return
+                case '-':
+                    OPERATERBUTTONS(button)
+                    return
+                case '+':
+                    OPERATERBUTTONS(button)
+                    return
+                case '=':
+                    if (!firstNumber && !secondNumber) {
+                        return
+                    }
+                    if (counter === false) {
+                        return
+                    }
+                    secondNumber = BUTTOMSCREEN.innerText
+                    TOPSCREEN.textContent = `${firstNumber} ${operator} ${secondNumber}`
+                    BUTTOMSCREEN.textContent = OPERATE(firstNumber,secondNumber,operator)
+                    firstNumber = BUTTOMSCREEN.textContent
+                    secondNumber, operator = ''
+                    counter = false
+                    return
+                case '+/-':
+                    BUTTOMSCREEN.textContent = parseInt(BUTTOMSCREEN.textContent) * (-1)
+                    return
+                default:
+                    BUTTOMSCREEN.textContent += button.innerText
+            }
+        })
     })
-})
-
-
+}
 
 
 
 RESET()
 DELETE()
+MAINBUTTONS()
